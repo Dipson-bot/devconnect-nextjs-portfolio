a2676f0eae6af1f30f0aa180cb25d9fd7a667f5f
Dipson Thapa <72684581+Dipson-bot@users.noreply.github.com>
2025-06-12 14:24:18 +0545

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,8 @@ export default function About() {
     <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-lg mt-10">
       <h1 className="text-4xl font-bold mb-6 text-gray-900">About Me</h1>
       <p className="text-lg text-gray-700 leading-relaxed">
-        This is the about page. You can add information about yourself here.
+         Dedicated computer engineering student currently in 8th semester, seeking an internship opportunity to apply tech
+nical knowledge and problem-solving skills in a dynamic environment.
       </p>
     </div>
   );
